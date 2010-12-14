96583c7541534ece569617f6b2e0818ec206a050
onedayitwillmake <mariogonzalez@gmail.com>
2010-12-13 22:41:20 -0500

    HelloWorld

--- js/main.js
+++ js/main.js
@@ -1,5 +1,34 @@
-require(['lib/caat', 'lib/Stats'], function() {
-	require.ready(function() {
-		console.log('Hello')
+require(['lib/caat', 'lib/Stats'], function()
+{
+	require.ready(function()
+	{
+
+		var director = new CAAT.Director().initialize(100, 100);
+		var scene = new CAAT.Scene().create();
+		var circle = new CAAT.ShapeActor().create();
+		circle.setLocation(20, 20).
+				setSize(60,60).
+				setFillStyle('#ff00ff').
+				setStrokeStyle('#00ff00');
+
+		scene.addChild(circle);
+		director.addScene(scene);
+
+
+		// Insert canvas into HTML
+		$(director.canvas).appendTo(  $('body') );
+		director.loop(1);
+		/**
+		 * Stats
+		 * Create stats module, and attach to top left
+		 */
+		var stats = new Stats();
+		stats.domElement.style.position = 'absolute';
+		stats.domElement.style.left = '0px';
+		stats.domElement.style.top = '0px';
+//		$(stats.domElement).appendTo( $('body') );
+		setInterval( function () {
+			stats.update();
+		}, 1000 / 30 );
 	})
 });
