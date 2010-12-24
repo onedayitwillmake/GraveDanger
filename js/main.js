73cf47d6f0ab3417e7be971ce0102d3536a9fc5a
onedayitwillmake <mariogonzalez@gmail.com>
2010-12-24 02:24:20 -0500

    Committing latest version

--- js/main.js
+++ js/main.js
@@ -4,24 +4,10 @@ require(['lib/caat', 'lib/Stats'], function()
 	{
 
 		var director = new CAAT.Director().initialize(900, 600);
-
-
 		initWithDirector(director);
 		// Insert canvas into HTML
 		$(director.canvas).appendTo(  $('body') );
 		director.loop(60);
-		/**
-		 * Stats
-		 * Create stats module, and attach to top left
-		 */
-		var stats = new Stats();
-		stats.domElement.style.position = 'absolute';
-		stats.domElement.style.left = '0px';
-		stats.domElement.style.top = '0px';
-		$(stats.domElement).appendTo( $('body') );
-		setInterval( function () {
-			stats.update();
-		}, 1000 / 30 );
 	})
 });
 
@@ -31,22 +17,53 @@ function initWithDirector(director)
 
 	for(var i = 0; i < 50; i++)
 	{
-		var circle = new CAAT.ShapeActor().create();
-		circle.setShape( CAAT.ShapeActor.prototype.SHAPE_RECTANGLE ).
+		var rectangleActor = new CAAT.ShapeActor().create();
+		rectangleActor.setShape( CAAT.ShapeActor.prototype.SHAPE_RECTANGLE ).
 				setLocation( Math.random() * director.canvas.width, Math.random() * director.canvas.height).
 				setSize(60,60).
 				setFillStyle('#ff00ff').
-				setStrokeStyle('#00ff00');
+				setStrokeStyle('#ff0000');
 
-		var alphaBehavior = new CAAT.AlphaBehavior();
-		alphaBehavior.startAlpha = 1.0;
-		alphaBehavior.endAlpha = 0.1;
-		alphaBehavior.setFrameTime(Math.random() * 500, Math.random() * 5000 + 500);
 
-		circle.addBehavior(alphaBehavior);
-
-		scene.addChild(circle);
+		var scaleBehavior = animateInUsingScale(rectangleActor, Math.random() * 1000, 1200, Math.random() * 5, 0);
+		scaleBehavior.setCycle(true);
+		scaleBehavior.setPingPong(true);
+		scene.addChild(rectangleActor);
 	}
 
 	director.addScene(scene);
+}
+
+/**
+ * Adds a CAAT.ScaleBehavior to the entity, used on animate in
+ */
+function animateInUsingScale(actor, starTime, endTime, startScale, endScale)
+{
+   var scaleBehavior = new CAAT.ScaleBehavior();
+	scaleBehavior.anchor = CAAT.Actor.prototype.ANCHOR_CENTER;
+	actor.scaleX = actor.scaleY = scaleBehavior.startScaleX = scaleBehavior.startScaleY = startScale;  // Fall from the 'sky' !
+	scaleBehavior.endScaleX = scaleBehavior.endScaleY = endScale;
+	scaleBehavior.setFrameTime( starTime, endTime );
+	scaleBehavior.setCycle(false);
+	scaleBehavior.setInterpolator( new CAAT.Interpolator().createBounceOutInterpolator(false) );
+	actor.addBehavior(scaleBehavior);
+
+	return scaleBehavior;
+}
+
+/**
+ * Adds a CAAT.ScaleBehavior to the entity, used on animate in
+ */
+function animateInUsingAlpha(actor, starTime, endTime, startAlpha, endAlpha)
+{
+   var fadeBehavior = new CAAT.AlphaBehavior();
+	fadeBehavior.anchor = CAAT.Actor.prototype.ANCHOR_CENTER;
+	actor.alpha = fadeBehavior.startAlpha = startAlpha;
+	fadeBehavior.endAlpha = endAlpha;
+	fadeBehavior.setFrameTime( starTime, endTime );
+	fadeBehavior.setCycle(false);
+	fadeBehavior.setInterpolator( new CAAT.Interpolator().createExponentialOutInterpolator(2, false) );
+	actor.addBehavior(fadeBehavior);
+
+	return fadeBehavior;
 }
